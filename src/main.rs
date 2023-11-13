c66c0ce052803fb039ace612d4afc712a73575c6
Jimmy Chen Chen <30018387+jchenche@users.noreply.github.com>
2023-11-12 16:57:55 -0800

    Added comments to explain some things

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod parsing;
 
 use theorem_prover::{lang::{Formula::*, Term::*, Pred, Var}, boxed};
-use parsing::parse_formula;
 
 fn main() {
-    // TODO: Parse input formula into F of type Formula and feed it into is_valid()
+    // TODO: Parse input formula into F of type Formula and feed it into is_valid(). We may assume input follows a form:
+    // Symbols are forall, exists, ~, /\, \/, ->, <->, true, false, and arbitrary letters p, f, c, x, Jane, etc...
+    // Every connectives always wrap their subformulas with parenthesis.
     // TODO: maybe use macros to make it easier to read and write formulas
     // F : ¬(∃y.(∀z.((p(z, y)) ↔ (¬(∃x.((p(z, x)) ∧ (p(x, z))))))))
     let pzy = Pred(Pred::new("p", vec![boxed!(Var(Var::new("z"))), boxed!(Var(Var::new("y")))]));
@@ -15,8 +16,8 @@ fn main() {
         Neg(boxed!(Exists(Var::new("y"), boxed!(Forall(Var::new("z"),
             boxed!(Iff(
                 boxed!(pzy),
-                boxed!(Neg(boxed!(Exists(Var::new("x"), boxed!(p_conj))))))
-        ))))));
+                boxed!(Neg(boxed!(Exists(Var::new("x"), boxed!(p_conj)))))))
+        )))));
 
     println!("{formula}");
     if theorem_prover::is_valid(formula) {
