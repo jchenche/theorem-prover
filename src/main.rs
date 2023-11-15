54fef79ccca1b62cdfae35218811efbbc17cd2cd
Jimmy Chen Chen <30018387+jchenche@users.noreply.github.com>
2023-11-15 00:49:35 -0800

    Create unit test for unification.rs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     // TODO: Parse input formula into F of type Formula and feed it into is_valid(). We may assume input follows a form:
     // Symbols are forall, exists, ~, /\, \/, ->, <->, true, false, and arbitrary letters p, f, c, x, Jane, etc...
     // Every connectives always wrap their subformulas with parenthesis so we don't have to deal with operator precedence
-    // F : ¬(∃y.(∀z.((p(z, y)) ↔ (¬(∃x.((p(z, x)) ∧ (p(x, z))))))))
+    // ¬(∃y.(∀z.((p(z, y)) ↔ (¬(∃x.((p(z, x)) ∧ (p(x, z))))))))
     let formula =
         Neg!(Exists!("y", Forall!("z",
             Iff!(
--- src/unification.rs
+++ src/unification.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use crate::{lang::{Var, Term, Formula, Obj}, Obj};
+use crate::{lang::{Var, Term, Formula, Fun, Obj}, Var, Fun, Obj};
 
 type Unifier = HashMap<Var, Term>;
 
@@ -8,6 +8,7 @@ pub fn most_general_unifier(formulas: Vec<Formula>) -> Unifier {
 
     let processed = vec![false; formulas.len()];
     unifier.insert(Var::new("x"), Obj!("a"));
+    unifier.insert(Var::new("y"), Fun!("f", [Var!("a")]));
 
     return unifier;
 }
@@ -18,9 +19,8 @@ pub fn substitute(formula: Formula, unifier: Unifier) -> Formula {
 
 #[cfg(test)]
 mod tests {
-    use crate::{lang::{Pred, Var, Fun, Obj}, Pred, Var, Fun, Obj};
-
     use super::*;
+    use crate::{lang::{Pred, Var, Fun, Obj}, Pred, Var, Fun, Obj};
 
     #[test]
     fn test_most_general_unifier_1() {
@@ -28,5 +28,6 @@ mod tests {
         let p_x_fx = Pred!("p", [Obj!("a"), Fun!("f", [Var!("x")])]); // p(x, f(x))
         let unifier = most_general_unifier(vec![p_a_y, p_x_fx]);
         assert_eq!(unifier.get(&Var::new("x")), Some(&Obj!("a")));
+        assert_eq!(unifier.get(&Var::new("y")), Some(&Fun!("f", [Var!("a")])));
     }
 }
