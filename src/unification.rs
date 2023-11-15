b88332ab5303d577f535312b88d46fd2badcc415
Jimmy Chen Chen <30018387+jchenche@users.noreply.github.com>
2023-11-14 20:48:00 -0800

    Add substitude pub fn in unification.rs

--- src/unification.rs
+++ src/unification.rs
@@ -2,8 +2,12 @@ use std::collections::HashMap;
 
 use crate::lang::{Var, Term, Formula};
 
-pub type Unifier = HashMap<Var, Term>;
+type Unifier = HashMap<Var, Term>;
 
 pub fn most_general_unifier(formulas: Vec<Formula>) -> Unifier {
+    todo!()
+}
+
+pub fn substitute(formula: Formula, unifier: Unifier) -> Box<Formula> {
     todo!()
 }
